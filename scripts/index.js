04e9bf41adce32fada4be5f2cd37a2260e268b7c
Natalia Davydova <dav.nata@mail.ru>
2025-06-16 02:42:55 +0500

    added media queries

--- scripts/index.js
+++ scripts/index.js
@@ -27,6 +27,10 @@ candleSeries1.setData([
   { time: "2023-01-05", open: 401, high: 411, low: 396, close: 410 },
   { time: "2023-01-06", open: 412, high: 423, low: 401, close: 414 },
   { time: "2023-01-07", open: 417, high: 422, low: 410, close: 418 },
+  { time: "2023-01-08", open: 418, high: 430, low: 406, close: 412 },
+  { time: "2023-01-09", open: 411, high: 417, low: 402, close: 416 },
+  { time: "2023-01-10", open: 418, high: 429, low: 407, close: 409 },
+  { time: "2023-01-11", open: 412, high: 426, low: 403, close: 419 },
 ]);
 
 const chartContainer2 = document.getElementById('candle-chart-2');
